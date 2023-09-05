b9f342eaeace902676e9c864d530df3eec2d0a3a
tolos123 <alemchu123@yahoo.com>
2023-09-05 21:50:01 +0100

    first commit

---
+++ cron-dailyVerseGenerator.js
@@ -0,0 +1,14 @@
+const cron = require('node-cron');
+const fs = require('fs');
+cron.schedule('* * * * *',() => {
+    console.log('after 1 min');
+    const verses = fs.readFileSync('database.txt', 'utf-8').split('\n').map(line => line.trim());
+    const randomIndex = Math.floor(Math.random() * verses.length);
+    const randomVerse = verses[randomIndex];
+    console.log(randomVerse);
+    fs.writeFileSync('DailyVerse.txt', randomVerse, err = {
+        if(err){
+            console.log('error occurred');
+        }
+    })
+})
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const bibleVerses = fs.readFileSync('database.txt', 'utf8').split('\n').map(line => line.trim());
+
+// endpoint 
+app.get('/api/random-verse', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * bibleVerses.length);
+    const randomVerse = bibleVerses[randomIndex];
+    res.json({ verse: randomVerse });
+});
+
+app.post('/api/add-verse', (req, res) => {
+    const content =req.body.verse;
+    console.log(req.body)
+
+    fs.appendFile('database.txt', content+"\r\n", err => {
+    if (err) {
+        console.log("An error occurred");
+    }
+    });
+    let bibleVerses = fs.readFileSync('database.txt', 'utf8').split('\n').map(line => line.trim());
+    res.json({ verse: bibleVerses });
+});
+
+app.get('/api/daily-verse', (req, res) => {
+    const verse = fs.readFileSync('DailyVerse.txt', 'utf8').split('\n').map(line => line.trim());
+    res.json({verse});
+})
+
+app.get('/', (req,res) => {
+	res.json({message: "Home"});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} `);
+});
